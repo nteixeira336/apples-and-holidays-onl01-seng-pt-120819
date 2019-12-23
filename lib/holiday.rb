e04ba7f72a8f63cb5b6953ba9ede40629acb78dc
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-23 01:10:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ add_new_holiday_with_supplies(holiday_supplies, :fall, :succas, ["lulav", "esrog
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  holiday_hash[:winter].values.flatten
 
 end
 
