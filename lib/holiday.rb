b4fc0334be0ec37cdd9ce9ca8bdbe67084519148
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-23 01:08:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,6 +45,7 @@ holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
 
+
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
